bec68017a078c0b91485195106771d6bfa21ad51
João Santos <jmcs@jsantos.eu>
2013-11-09 00:24:03 +0100

    - Added local testing server implementation

--- example.py
+++ example.py
@@ -5,3 +5,8 @@ from object_rest import Service
 openweathermap = Service("http://api.openweathermap.org/data/2.5")
 print(openweathermap.weather(q='Berlin,DE'))
 print(openweathermap['weather'](q='Berlin,DE'))
+
+local = Service("http://127.0.0.1:5000")
+print(local.test(param1=1, param2='b'))
+print(local.test.abc("POST", param1=1, param2='b'))
+local.test.cde = {'v': 4}
---
+++ testing/server.py
@@ -0,0 +1,14 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>', methods=['GET', 'HEAD', 'POST', 'PUT'])
+def catch_all(path):
+    values = {'path': path,
+              'method': flask.request.method,
+              'values': flask.request.args}
+    return flask.json.jsonify(values)
+
+if __name__ == '__main__':
+    app.run()
