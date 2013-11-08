bae01cb90036eb9a72b594523afee55d75783011
João Santos <jmcs@jsantos.eu>
2013-11-08 23:36:39 +0100

    - PUT request on set - Simplification

--- object_rest.py
+++ object_rest.py
@@ -1,13 +1,12 @@
+import configparser
 import requests
 import json
 
 
 class Node(object):
-    def __init__(self, url, suffix="", session=None):
+    def __init__(self, url, session=None):
         self.__url = url
         self.__session = session
-        self.__method = "GET"
-        self.__suffix = suffix
         self.__children = dict()
 
     def __getattr__(self, name):
@@ -22,7 +21,7 @@ class Node(object):
         """
         if name not in self.__children:
             child_url = "{base_url}/{name}".format(base_url=self.__url, name=name)
-            self.__children[name] = Node(child_url, self.__suffix, self.__session)
+            self.__children[name] = Node(child_url, self.__session)
         return self.__children[name]
 
     def __getitem__(self, name):
@@ -37,15 +36,28 @@ class Node(object):
         """
         return self.__getattr__(name)
 
-    def __call__(self, __method=None, **kwargs):
-        method = __method if __method else self.__method
-        url = self.__url + self.__suffix
+    def __setattr__(self, key, value):
+        """
+        Put object
 
-        if method == "GET":
+        :param key: Node name
+        :type key: str
+        :param value: Object Value
+        :type value: dict
+        :return: None
+        """
+        if key.startswith('_Node__'):
+            self.__dict__[key] = value
+        else:
+            child_url = "{base_url}/{name}".format(base_url=self.__url, name=key)
+            self.__session.put(child_url, params=value)
+
+    def __call__(self, __method="GET", **kwargs):
+        if __method == "GET":
             method = self.__session.get
-        elif method == "POST":
+        elif __method == "POST":
             method = self.__session.post
-        response = method(url, params=kwargs)
+        response = method(self.__url, params=kwargs)
         try:
             return json.loads(response.content.decode('utf-8'))
         except ValueError:
@@ -53,8 +65,8 @@ class Node(object):
 
 
 class Service(Node):
-    def __init__(self, url, suffix=""):
+    def __init__(self, url):
         session = requests.Session()
         session.headers = {'user-agent': 'jmcs\'s test rest module', }
-        super(Service, self).__init__(url, suffix, requests.Session())
+        super(Service, self).__init__(url, requests.Session())
 
