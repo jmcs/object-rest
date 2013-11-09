45baf9a354685ee40f2e8fe58528d6fcec3396f7
João Santos <jmcs@jsantos.eu>
2013-11-09 01:50:59 +0100

    - Allow defining a documentation to specify the default method for each path

--- example.py
+++ example.py
@@ -10,3 +10,7 @@ local = Service("http://127.0.0.1:5000")
 print(local.test(param1=1, param2='b'))
 print(local.test.abc("POST", param1=1, param2='b'))
 local.test.cde = {'v': 4}
+
+local_with_config = Service("http://127.0.0.1:5000", documentation="config/service.txt")
+local_with_config.test()
+local_with_config.without()
--- object_rest.py
+++ object_rest.py
@@ -1,12 +1,14 @@
-import configparser
+from collections import defaultdict
 import requests
 import json
-
+import urllib.parse
 
 class Node(object):
-    def __init__(self, url, session=None):
+    def __init__(self, url, session=None, documentation={}):
         self.__url = url
         self.__session = session
+        self.__path = urllib.parse.urlparse(url).path
+        self.__documentation = documentation
         self.__children = dict()
 
     def __getattr__(self, name):
@@ -21,13 +23,13 @@ class Node(object):
         """
         if name not in self.__children:
             child_url = "{base_url}/{name}".format(base_url=self.__url, name=name)
-            self.__children[name] = Node(child_url, self.__session)
+            self.__children[name] = Node(child_url, self.__session, self.__documentation)
         return self.__children[name]
 
     def __getitem__(self, name):
         """
-        Access child node using dict like syntax for node names that can't be fetched as an attribute
-        like numbers or reserved attributes likes __method
+        Access child node using dict like syntax for node names that can't be fetched as an
+        attribute like numbers or reserved attributes likes __method
 
         :param name: Node name
         :type name: str
@@ -46,13 +48,22 @@ class Node(object):
         :type value: dict
         :return: None
         """
-        if key.startswith('_Node__'):
+
+        #todo add __set_key__
+        if key.startswith('_'):
             self.__dict__[key] = value
         else:
             child_url = "{base_url}/{name}".format(base_url=self.__url, name=key)
             self.__session.put(child_url, params=value)
 
-    def __call__(self, __method="GET", **kwargs):
+    def __call__(self, __method=None, **kwargs):
+        #When the method isn't specified
+        if not __method:
+            if self.__path in self.__documentation:
+                #When not specified we use the first documented method
+                __method = self.__documentation[self.__path][0]
+            else:
+                __method = "GET"
         if __method == "GET":
             method = self.__session.get
         elif __method == "POST":
@@ -65,8 +76,23 @@ class Node(object):
 
 
 class Service(Node):
-    def __init__(self, url):
+
+    @staticmethod
+    def __parse_documentation(fname):
+        #todo: catch errors
+        config = defaultdict(list)
+        with open(fname) as cfg_file:
+                for line in cfg_file:
+                    if not line or line[0].isspace():
+                        continue
+                    method, url = line.split()
+                    config[url].append(method)
+        return config
+
+    def __init__(self, url, documentation=None):
+        #todo: allow defining only the documentation and extract the url from it
         session = requests.Session()
-        session.headers = {'user-agent': 'jmcs\'s test rest module', }
-        super(Service, self).__init__(url, requests.Session())
+        session.headers = {'user-agent': 'object_rest.py', }
+        doc = Service.__parse_documentation(documentation) if documentation else {}
+        super(Service, self).__init__(url, requests.Session(), doc)
 
