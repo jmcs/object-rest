67373af20a766481f0940280b500541ae7e1d82c
João Santos <jmcs@jsantos.eu>
2013-11-12 20:23:05 +0100

    - Separate documentation parser from service module

--- object_rest/service.py
+++ object_rest/service.py
@@ -1,76 +1,7 @@
-from collections import defaultdict
 import requests
 import json
 import urllib.parse
-
-
-class Page(object):
-    def __init__(self):
-        self.methods = []
-        self.description = ''
-
-    @property
-    def default_method(self):
-        """
-        Returns first defined access method if present on configuration or 'GET' otherwise
-        :return: Default method
-        :rtype: str
-        """
-        return self.methods[0] if self.methods else 'GET'
-
-
-class Documentation(object):
-    """
-    Object to hold all the service documentation
-    """
-
-    def __init__(self, fname=None):
-        self.config = defaultdict(Page)  # Service Documentation
-        self.params = dict()  # General service configuration
-        if fname:
-            self.parse(fname)
-
-    def __getitem__(self, path):
-        """
-        Returns documentation for path, with helpful defaults.
-
-        :param path: Path
-        :return: Documentation for path
-        :rtype: namedtuple
-        """
-
-        page = self.config[path]
-        return page
-
-    def parse(self, fname):
-        if not fname:
-            #If there is no config file return with empty config
-            return
-        with open(fname) as cfg_file:
-            for line in cfg_file:
-                if not line.strip():  # if line is empty we can ignore it
-                    continue
-                if line.startswith(':'):  # it's a parameter
-                    key, value = line[1:].split(':', maxsplit=1)
-                    self.params[key] = value.strip()
-                    continue
-
-                if not line[0].isspace():  # if the first char isn't whitespace then it should
-                                           # be rule
-                    method, url = line.split()
-                    self.config[url].methods.append(method)
-                else:
-                    self.config[url].description += line.strip()
-
-    @staticmethod
-    def help(node):
-        """
-        Prints Node help
-        :param node: Node to inspect
-        """
-        page = node._Node__doc_page # We need to access to the private property to get
-                                    # the documentation
-        print(page.description)
+from object_rest.documentation import Documentation
 
 
 class Node(object):
@@ -187,7 +118,6 @@ class Node(object):
 
 
 class Service(Node):
-
     def __init__(self, url=None, documentation=None):
         session = requests.Session()
         session.headers = {'user-agent': 'object_rest.py', }
@@ -199,6 +129,6 @@ class Service(Node):
         super(Service, self).__init__(url, requests.Session(), doc)
 
 
-#todo: move everything to folder, separate documentation and service
-#TODO: Add documentation to the method (implies replacing simple method in the list with a dict)
-#TODO: Optional parts of the url on documentation (like reddit API)
+        #todo: move everything to folder, separate documentation and service
+        #TODO: Add documentation to the method (implies replacing simple method in the list with a dict)
+        #TODO: Optional parts of the url on documentation (like reddit API)
